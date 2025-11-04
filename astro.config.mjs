758399938a016c460205dc9069203f3fbeb8363e
kevindwi <kevindwi356@gmail.com>
2025-11-04 23:17:42 +0700

    fix: url missmatch

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kevindwi.github.io",
+  base: "/blog-app",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
