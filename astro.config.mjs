61e310b43ed0b8de968cfd45fea3096792aa2d04
kevindwi <kevindwi356@gmail.com>
2025-11-20 13:30:42 +0700

    feat: add integration with expressive-code, katex & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,20 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import expressiveCode from "astro-expressive-code";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kevindwi.github.io",
-  base: "/blog-app",
-  integrations: [react()],
+  integrations: [react(), expressiveCode(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
